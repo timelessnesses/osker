573408ec403b4d16eb330526fb81fa3a682bec51
timelesnesses <timelessnesses@timelessnesses.me>
2024-07-20 18:29:16 +0700

    backend done

--- tlns-plotter/src/lib.rs
+++ tlns-plotter/src/lib.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Read;
+
+use plotly;
+use cuid2;
+
+pub fn plot<const N: usize>(data: [String; N], thetas: [String; N]) -> std::io::Bytes<std::io::BufReader<std::fs::File>> {
+    let sc = plotly::ScatterPolar::new(thetas.to_vec(), data.to_vec());
+    let mut pl = plotly::Plot::new();
+    pl.add_trace(sc);
+    let file = "./".to_string() + &get_random_id_string() + ".png";
+    pl.write_image(&file, plotly::ImageFormat::PNG, 1280, 720, 1.0);
+    let file = std::fs::OpenOptions::new().read(true).open(file).expect("Failed to open file");
+    std::io::BufReader::new(file).bytes()
+}
+
+fn get_random_id_string() -> String {
+    cuid2::create_id()
 }
--- tlns-tetrio-calcs/src/lib.rs
+++ tlns-tetrio-calcs/src/lib.rs
@@ -10,8 +10,8 @@ pub struct ProfileStats {
     pub vs: f32,
     pub rank: Ranks,
     pub tr: f32,
-    pub name: String,
-    pub pfp: String,
+    pub name: Option<String>,
+    pub pfp: Option<String>,
     pub glicko: f64,
     pub rd: f64,
 }
@@ -100,11 +100,13 @@ impl ProfileStats {
             rank: Ranks::from_str(&response["league"]["rank"].as_str().unwrap().to_uppercase())
                 .unwrap(),
             tr: response["league"]["tr"].as_f64().unwrap() as f32,
-            name: response["username"].as_str().unwrap().to_string(),
-            pfp: "https://tetr.io/user-content/avatars/".to_string()
-                + response["_id"].as_str().unwrap()
-                + ".jpg?rv="
-                + response["avatar_revision"].as_str().unwrap(),
+            name: Some(response["username"].as_str().unwrap().to_string()),
+            pfp: Some(
+                "https://tetr.io/user-content/avatars/".to_string()
+                    + response["_id"].as_str().unwrap()
+                    + ".jpg?rv="
+                    + response["avatar_revision"].as_str().unwrap(),
+            ),
             rd: response["league"]["rd"].as_f64().unwrap(),
             glicko: response["league"]["glicko"].as_f64().unwrap(),
         })
@@ -144,12 +146,12 @@ impl ProfileStats {
 
     #[inline(always)]
     pub fn area(&self) -> f64 {
-        self.apm as f64 * weights::APM_WEIGHT as f64
+        self.apm as f64
             + self.pps as f64 * weights::PPS_WEIGHT as f64
             + self.vs as f64 * weights::VS_WEIGHT as f64
             + self.app() * weights::APP_WEIGHT as f64
             + self.ds_seconds() * weights::DS_SECONDS_WEIGHT as f64
-            + self.ds_pieces() * weights::DS_SECONDS_WEIGHT as f64
+            + self.ds_pieces() * weights::DS_PIECES_WEIGHT as f64
             + self.garbage_efficiency() * weights::GARBAGE_EFFICIENCY_WEIGHT as f64
     }
 
@@ -168,14 +170,15 @@ impl ProfileStats {
     pub fn sr(&self) -> f64 {
         let x = (11.2 * ((self.sr_area() - 93.0) / 130.0).atan()) + 1.0;
         if x <= 0.0 {
-            return 0.001
+            return 0.001;
         }
         x
     }
 
     #[inline(always)]
     pub fn weighted_app(&self) -> f64 {
-        self.app() - 5.0 * ((self.cheese_index() / -30.0) + 1.0 * core::f64::consts::PI / 180.0).tan()
+        self.app()
+            - 5.0 * (((self.cheese_index() / -30.0) + 1.0) * core::f64::consts::PI / 180.0).tan()
     }
 
     #[inline(always)]
@@ -185,34 +188,126 @@ impl ProfileStats {
 
     #[inline(always)]
     pub fn opener(&self) -> f64 {
-        todo!()
+        ((((self.apm as f64 / self.sr_area())
+            / ((0.069 * 1.0017_f64.powf((self.sr().powi(5)) / 4700.0)) + self.sr() / 360.0)
+            - 1.0)
+            + (((self.pps as f64 / self.sr_area())
+                / (0.0084264 * (2.14_f64.powf(-2.0 * (self.sr() / 2.7 + 1.03)))
+                    - self.sr() / 5750.0
+                    + 0.0067)
+                - 1.0)
+                * 0.75)
+            + ((self.vs_apm() / (-(((self.sr() - 16.0) / 36.0).powi(2)) + 2.133) - 1.0) * -10.0)
+            + ((self.app()
+                / (0.1368803292 * 1.0024_f64.powf((self.sr().powi(5)) / 2800.0)
+                    + self.sr() / 54.0)
+                - 1.0)
+                * 0.75)
+            + ((self.ds_pieces()
+                / (0.02136327583 * (14.0_f64.powf((self.sr() - 14.75) / 3.9))
+                    + self.sr() / 152.0
+                    + 0.022)
+                - 1.0)
+                * -0.25))
+            / 3.5)
+            + 0.5
     }
 
     #[inline(always)]
     pub fn plonk(&self) -> f64 {
-        todo!()
+        let x = (((self.garbage_efficiency()
+            / (self.sr() / 350.0
+                + 0.005948424455 * 3.8_f64.powf((self.sr() - 6.1) / 4.0)
+                + 0.006)
+            - 1.0)
+            + (self.app()
+                / (0.1368803292 * 1.0024_f64.powf((self.sr().powi(5)) / 2800.0)
+                    + self.sr() / 54.0)
+                - 1.0)
+            + ((self.ds_pieces()
+                / (0.02136327583 * (14.0_f64.powf((self.sr() - 14.75) / 3.9))
+                    + self.sr() / 152.0
+                    + 0.022)
+                - 1.0)
+                * 0.75)
+            + (((self.pps as f64 / self.sr_area())
+                / (0.0084264 * (2.14_f64.powf(-2.0 * (self.sr() / 2.7 + 1.03)))
+                    - self.sr() / 5750.0
+                    + 0.0067)
+                - 1.0)
+                * -1.0))
+            / 2.73)
+            + 0.5;
+        truncate(x, 4)
     }
 
     #[inline(always)]
     pub fn stride(&self) -> f64 {
-        todo!()
+        let x = (((((self.apm as f64 / self.sr_area())
+            / ((0.069 * 1.0017_f64.powf((self.sr().powi(5)) / 4700.0)) + self.sr() / 360.0)
+            - 1.0)
+            * -0.25)
+            + ((self.pps as f64 / self.sr_area())
+                / (0.0084264 * (2.14_f64.powf(-2.0 * (self.sr() / 2.7 + 1.03)))
+                    - self.sr() / 5750.0
+                    + 0.0067)
+                - 1.0)
+            + ((self.app()
+                / (0.1368803292 * 1.0024_f64.powf((self.sr().powi(5)) / 2800.0)
+                    + self.sr() / 54.0)
+                - 1.0)
+                * -2.0)
+            + ((self.ds_pieces()
+                / (0.02136327583 * (14.0_f64.powf((self.sr() - 14.75) / 3.9))
+                    + self.sr() / 152.0
+                    + 0.022)
+                - 1.0)
+                * -0.5))
+            * 0.79)
+            + 0.5;
+        truncate(x, 4)
     }
 
     #[inline(always)]
     pub fn infinite_downstack(&self) -> f64 {
-        todo!()
+        let x = (((self.ds_pieces()
+            / (0.02136327583 * (14.0_f64.powf((self.sr() - 14.75) / 3.9))
+                + self.sr() / 152.0
+                + 0.022)
+            - 1.0)
+            + ((self.app()
+                / (0.1368803292 * 1.0024_f64.powf((self.sr().powi(5)) / 2800.0)
+                    + self.sr() / 54.0)
+                - 1.0)
+                * -0.75)
+            + (((self.apm as f64 / self.sr_area())
+                / ((0.069 * 1.0017_f64.powf((self.sr().powi(5)) / 4700.0)) + self.sr() / 360.0)
+                - 1.0)
+                * 0.5)
+            + ((self.vs_apm() / (-(((self.sr() - 16.0) / 36.0).powi(2)) + 2.133) - 1.0) * 1.5)
+            + (((self.pps as f64 / self.sr_area())
+                / (0.0084264 * (2.14_f64.powf(-2.0 * (self.sr() / 2.7 + 1.03)))
+                    - self.sr() / 5750.0
+                    + 0.0067)
+                - 1.0)
+                * 0.5))
+            * 0.9)
+            + 0.5;
+
+        truncate(x, 4)
     }
 
     #[inline(always)]
-    // Est. TR:
     pub fn estimated_tr(&self) -> f64 {
         25000.0
             / (1.0
-                + (10.0 as f64).powf(
+                + 10.0_f64.powf(
                     ((1500.0 - self.estimated_glicko()) * core::f64::consts::PI)
-                        / (((3.0 * core::f64::consts::LOG10_E).powi(2) * 60.0).powi(2)).sqrt()
-                        + (2500.0 * (64.0 * core::f64::consts::PI).powi(2)
-                            + (147.0 * core::f64::consts::LOG10_E).powi(2)),
+                        / ((((3.0 * 10.0_f64.ln().powi(2)) * 60.0_f64.powi(2))
+                            + (2500.0
+                                * ((64.0 * core::f64::consts::PI.powi(2))
+                                    + (147.0 * 10.0_f64.ln().powi(2)))))
+                        .sqrt()),
                 ))
     }
 
@@ -239,4 +334,133 @@ impl ProfileStats {
     pub fn vs_apm(&self) -> f64 {
         self.vs as f64 / self.apm as f64
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use once_cell::sync::OnceCell;
+
+    use super::*;
+
+    static DATA: once_cell::sync::OnceCell<ProfileStats> = OnceCell::with_value(ProfileStats {
+        apm: 66.09,
+        pps: 2.07,
+        vs: 135.65,
+        rank: Ranks::U,
+        tr: 23684.48,
+        name: None,
+        pfp: None,
+        glicko: 2257.86,
+        rd: 66.04,
+    });
+
+    #[test]
+    fn test_ds_pieces() {
+        let a = DATA.wait();
+        assert_eq!(truncate(a.ds_pieces(), 4), 0.1232);
+    }
+
+    #[test]
+    fn test_app() {
+        let a = DATA.wait();
+        assert_eq!(truncate(a.app(), 4), 0.5321);
+    }
+
+    #[test]
+    fn test_app_ds_per_pieces() {
+        let a = DATA.wait();
+        assert_eq!(truncate(a.app_ds_per_pieces(), 4), 0.6553);
+    }
+
+    #[test]
+    fn test_ds_seconds() {
+        let a = DATA.wait();
+        assert_eq!(truncate(a.ds_seconds(), 4), 0.2550)
+    }
+
+    #[test]
+    fn test_vs_apm() {
+        let a = DATA.wait();
+        assert_eq!(truncate(a.vs_apm(), 4), 2.0525)
+    }
+
+    #[test]
+    fn test_garbage_eff() {
+        let a = DATA.wait();
+        assert_eq!(truncate(a.garbage_efficiency(), 4), 0.1311)
+    }
+
+    #[test]
+    fn test_cheese() {
+        let a = DATA.wait();
+        assert_eq!(truncate(a.cheese_index(), 4), 29.5878)
+    }
+
+    #[test]
+    fn test_weighted_app() {
+        let a = DATA.wait();
+        assert_eq!(truncate(a.weighted_app(), 4), 0.5309)
+    }
+
+    #[test]
+    fn test_area() {
+        let a = DATA.wait();
+        assert_eq!(truncate(a.area(), 4), 459.2692)
+    }
+
+    #[test]
+    fn test_est_tr() {
+        let a = DATA.wait();
+        assert_eq!(truncate(a.estimated_tr(), 2), 23747.64)
+    }
+
+    #[test]
+    fn test_est_glicko() {
+        let a = DATA.wait();
+        assert_eq!(
+            truncate(a.estimated_glicko(), 4),
+            truncate(2270.1577831407817, 4)
+        )
+    }
+
+    #[test]
+    fn test_sr_area() {
+        let a = DATA.wait();
+        assert_eq!(truncate(a.sr_area(), 4), truncate(519.998309178744, 4))
+    }
+
+    #[test]
+    fn test_sr() {
+        let a = DATA.wait();
+        assert_eq!(truncate(a.sr(), 4), truncate(15.282925422083864, 4))
+    }
+
+    #[test]
+    fn test_opener() {
+        let a = DATA.wait();
+        assert_eq!(truncate(a.opener(), 4), 0.5883)
+    }
+
+    #[test]
+    fn test_plonk() {
+        let a = DATA.wait();
+        assert_eq!(truncate(a.plonk(), 4), 0.3368)
+    }
+
+    #[test]
+    fn test_stride() {
+        let a = DATA.wait();
+        assert_eq!(truncate(a.stride(), 4), 0.6631)
+    }
+
+    #[test]
+    fn test_inf_ds() {
+        let a = DATA.wait();
+        assert_eq!(truncate(a.infinite_downstack(), 4), 0.2741)
+    }
+}
+
+pub fn truncate(b: f64, precision: usize) -> f64 {
+    let factor = 10f64.powi(precision as i32);
+    (b * factor).round() / factor
 }
